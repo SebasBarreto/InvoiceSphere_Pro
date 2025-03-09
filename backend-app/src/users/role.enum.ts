7b055b1fb876d5d99af0e3c60f4a60c5cbc43e95
SebasBarreto <shebasbt@gmail.com>
2025-03-09 14:48:01 -0500

    delete

--- backend-app/src/users/role.enum.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Enum representing the available roles for users.
- * Used for role-based access control (RBAC).
- */
-export enum Role {
-  Admin = 'admin', // Full access
-  User = 'user',   // Restricted access
-}
--- backend-app/src/users/user.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-import { Role } from './role.enum';  // Enum for user roles
-
-export interface User extends Document {
-  readonly name: string;
-  readonly email: string;
-  readonly password: string;
-  readonly role: Role;  // Role from the Role enum for type safety
-}
