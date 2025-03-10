f43f4ec0447c69d7b41b75d8a7f0feb2c938ab40
SebasBarreto <shebasbt@gmail.com>
2025-03-10 00:24:23 -0500

    (fix) arreglo carpeta frontend

--- backend-app/src/main.ts
+++ backend-app/src/main.ts
@@ -7,17 +7,17 @@ async function bootstrap() {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
 
-  // Usa el filtro global para capturar todas las excepciones
+ 
   app.useGlobalFilters(new AllExceptionsFilter());
 
   // Configuración de CORS
   app.enableCors({
-    origin: '*',  // Cambia esto si quieres restringir el acceso a ciertos dominios
+    origin: '*',  
     methods: 'GET, POST, PUT, DELETE, OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
-  // Inicia el servidor
+  
   await app.listen(3000);
 }
 bootstrap();
